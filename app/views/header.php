e84755bb28173c71e6af7b4df911ae37be3df87b
hatruong <hoanganhtruong0803@gmail.com>
2024-11-08 14:34:58 +0700

    giao dien

---
+++ app/views/header.php
@@ -0,0 +1,7 @@
+<header class="header border-bottom p-3 d-flex align-items-center justify-content-between">
+    <img src="image/CTU_logo.png" alt="logo" width="100px" class="logo">
+    <div class="container text-center">
+        <h1 class="tieude">QUẢN LÝ KÝ TÚC XÁ</h1>
+    </div>
+
+</header>
