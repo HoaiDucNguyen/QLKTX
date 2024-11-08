fd88b891ee732b2c8b1e5aa5d877a1572a61619d
hatruong <hoanganhtruong0803@gmail.com>
2024-11-09 01:20:15 +0700

    giao dien 2

--- app/views/nav.php
+++ app/views/nav.php
@@ -1,33 +1,3 @@
-
-<!-- nav.php -->
-<!-- <nav class="navbar navbar-expand-lg"> 
-    <a class="navbar-brand" href="#">Quản Lý Ký Túc Xá</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav flex-column">
-            <li class="nav-item">
-                <a class="nav-link" href="/phong">Quản Lý Phòng</a>
-            </li>
-            <li class="nav-item">
-                <?php if ($_SESSION['ghi_chu'] === 'admin') : ?>
-                    <a class="nav-link" href="/nhanvien">Quản Lý Nhân Viên</a>
-                <?php endif; ?>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/thuephong">Quản Lý Thuê Phòng</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/lop">Quản Lý Lớp</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/sinhvien">Quản Lý Sinh Vien</a>
-            </li>
-           
-        </ul>
-    </div>
-</nav> -->
 <?php
 function isActive($path) {
     $currentPath = $_SERVER['REQUEST_URI'];
@@ -47,8 +17,10 @@ function isActive($path) {
                         class="fas fa-tachometer-alt me-2"></i>Dashboard</a>
                 <a href="/phong" class="list-group-item list-group-item-action <?php echo isActive('/phong'); ?>"><i
                         class="fas fa-bed me-2"></i>Quản lý Phòng</a>
-                <a href="/nhanvien" class="list-group-item list-group-item-action <?php echo isActive('/nhanvien'); ?>"><i
+                <?php if ($_SESSION['ghi_chu'] === 'admin') : ?>
+                    <a href="/nhanvien" class="list-group-item list-group-item-action <?php echo isActive('/nhanvien'); ?>"><i
                         class="fas fa-users me-2"></i>Quản Lý Nhân Viên</a>
+                <?php endif; ?>
                 <a href="/thuephong" class="list-group-item list-group-item-action <?php echo isActive('/thuephong'); ?>"><i
                         class="fas fa-paperclip me-2"></i>Quản Lý Thuê Phòng</a>
                 <a href="/lop" class="list-group-item list-group-item-action <?php echo isActive('/lop'); ?>"><i
