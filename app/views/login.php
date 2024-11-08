99bb631cbcc0b685f141ca2c52eef4684c74628f
HoaiDuc <ducb2111881@student.ctu.edu.vn>
2024-11-09 02:09:20 +0700

    fix Sinh vien- nhan vien

--- app/views/login.php
+++ app/views/login.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Đăng Nhập</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link rel="stylesheet" type="text/css" href="style.css"> 
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
+
 <body>
     <div class="container-fluid mt-3">
         <div class="row">
@@ -21,13 +23,13 @@
                     THÀNH VIÊN ĐĂNG NHẬP
                 </div>
                 <?php if (!empty($error)): ?>
-                    <div class="alert alert-danger"><?= $error ?></div>
+                <div class="alert alert-danger"><?= $error ?></div>
                 <?php endif; ?>
                 <div class="card-body ">
                     <form class="form-horizontal" method="POST">
                         <div class="form-group">
-                            <label>Số Điện Thoại:</label>
-                            <input type="text" name="so_dien_thoai" class="form-control" required>
+                            <label>Mã số</label>
+                            <input type="text" name="ma_so" class="form-control" required>
                         </div>
                         <div class="form-group">
                             <label>Mật Khẩu:</label>
@@ -37,8 +39,9 @@
                     </form>
                 </div>
             </div>
-        </div> 
-                
+        </div>
+
     </div>
 </body>
+
 </html>
--- app/views/phong/index.php
+++ app/views/phong/index.php
@@ -8,7 +8,7 @@
     <title>Quản Lý Phòng</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link rel="stylesheet" type="text/css" href="style.css"> 
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
 <body>
@@ -17,7 +17,7 @@
             <div class="col-md-12">
                 <?php include '../app/views/header.php'; ?>
             </div>
-        </div>  
+        </div>
         <div class="row">
             <div class="col-md-2">
                 <?php include '../app/views/nav.php'; ?>
